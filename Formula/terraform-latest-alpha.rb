d25a553c4f3555a6a2ee7b1af34812c413a8e917
yakdriverbot <f1formula.juan@gmail.com>
2024-11-05 17:15:55 +0000

    Update Terraform formulas to alpha/beta/stable versions 1.10.0-alpha20241023/1.10.0-beta1/1.9.8

--- Formula/terraform-latest-alpha.rb
+++ Formula/terraform-latest-alpha.rb
@@ -1,31 +1,31 @@
 class TerraformLatestAlpha < Formula
   desc "Terraform Latest Alpha"
   homepage "https://www.terraform.io/"
-  version "1.10.0-alpha1"
+  version "1.10.0-alpha20241023"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/terraform/1.10.0-alpha1/terraform_1.10.0-alpha1_darwin_amd64.zip"
-    sha256 "66e69b5580907254f8bcb2b5cbec8c2f192ba076fcc7e4a14d9545d7c105c30d"
+    url "https://releases.hashicorp.com/terraform/1.10.0-alpha20241023/terraform_1.10.0-alpha20241023_darwin_amd64.zip"
+    sha256 "620b9be1be9e207d03a1df89fe90663665651d431b643ba73bb6a2fb542821dc"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://releases.hashicorp.com/terraform/1.10.0-alpha1/terraform_1.10.0-alpha1_darwin_arm64.zip"
-    sha256 "627dca78609e049ecee78b3e4c19a2c0e9dcc5a7c2b5538b60e7d3546bcf1423"
+    url "https://releases.hashicorp.com/terraform/1.10.0-alpha20241023/terraform_1.10.0-alpha20241023_darwin_arm64.zip"
+    sha256 "a07b1a120439d2f93f079d95a6570ece50bb3fef729a238857c654dcf2fb27b6"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/terraform/1.10.0-alpha1/terraform_1.10.0-alpha1_linux_amd64.zip"
-    sha256 "ec1e72dd60b3e327c7bdc5df3c8d1c6d86f953321751113e89536d73ce2b837f"
+    url "https://releases.hashicorp.com/terraform/1.10.0-alpha20241023/terraform_1.10.0-alpha20241023_linux_amd64.zip"
+    sha256 "6b78b525637aa57c4228cbd9992919a49b0a2393ed77455eba04e485e1fdd8a3"
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/terraform/1.10.0-alpha1/terraform_1.10.0-alpha1_linux_arm.zip"
-    sha256 "ac519ed9d6a967d2b839017d99c06aa5a0589ac6f658137e33f8b2385a878e20"
+    url "https://releases.hashicorp.com/terraform/1.10.0-alpha20241023/terraform_1.10.0-alpha20241023_linux_arm.zip"
+    sha256 "a9bfcae6b8234a8e33c4bbddb605c8e81b00724cdf553ac281c11a4665e4b94c"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/terraform/1.10.0-alpha1/terraform_1.10.0-alpha1_linux_arm64.zip"
-    sha256 "fe24716609db030b639ff8e1728271084394de57ba010d159fba732f347c2d39"
+    url "https://releases.hashicorp.com/terraform/1.10.0-alpha20241023/terraform_1.10.0-alpha20241023_linux_arm64.zip"
+    sha256 "92e2aa831171d13807e22d6be9d1cdd87a4f45d83d9ca32dc833ad2d260b02a7"
   end
 
   conflicts_with "terraform"
