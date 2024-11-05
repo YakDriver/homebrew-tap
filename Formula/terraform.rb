679aa3c12b814e1119769271794a434fe4f956f3
yakdriverbot <f1formula.juan@gmail.com>
2024-11-05 00:36:25 +0000

    Update Terraform formulas to beta/stable versions 1.10.0-beta1/1.9.8

--- Formula/terraform.rb
+++ Formula/terraform.rb
@@ -1,31 +1,31 @@
 class Terraform < Formula
   desc "Terraform"
   homepage "https://www.terraform.io/"
-  version "1.9.1"
+  version "1.9.8"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/terraform/1.9.1/terraform_1.9.1_darwin_amd64.zip"
-    sha256 "66e69b5580907254f8bcb2b5cbec8c2f192ba076fcc7e4a14d9545d7c105c30d"
+    url "https://releases.hashicorp.com/terraform/1.9.8/terraform_1.9.8_darwin_amd64.zip"
+    sha256 "be591e8c59c49d0cfbc7664d24910a4b43840b89d0a4bbca662149bbf0397e91"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://releases.hashicorp.com/terraform/1.9.1/terraform_1.9.1_darwin_arm64.zip"
-    sha256 "627dca78609e049ecee78b3e4c19a2c0e9dcc5a7c2b5538b60e7d3546bcf1423"
+    url "https://releases.hashicorp.com/terraform/1.9.8/terraform_1.9.8_darwin_arm64.zip"
+    sha256 "873d7b925d08578fb6bb9c12c7cd92ae73e289e07c360f2fdd69f9036b7baaab"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/terraform/1.9.1/terraform_1.9.1_linux_amd64.zip"
-    sha256 "ec1e72dd60b3e327c7bdc5df3c8d1c6d86f953321751113e89536d73ce2b837f"
+    url "https://releases.hashicorp.com/terraform/1.9.8/terraform_1.9.8_linux_amd64.zip"
+    sha256 "186e0145f5e5f2eb97cbd785bc78f21bae4ef15119349f6ad4fa535b83b10df8"
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/terraform/1.9.1/terraform_1.9.1_linux_arm.zip"
-    sha256 "ac519ed9d6a967d2b839017d99c06aa5a0589ac6f658137e33f8b2385a878e20"
+    url "https://releases.hashicorp.com/terraform/1.9.8/terraform_1.9.8_linux_arm.zip"
+    sha256 "cb6db8471e361bb9ad6bbd43d9c780d37208e6dbe416900fdc8999af9e459b77"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/terraform/1.9.1/terraform_1.9.1_linux_arm64.zip"
-    sha256 "fe24716609db030b639ff8e1728271084394de57ba010d159fba732f347c2d39"
+    url "https://releases.hashicorp.com/terraform/1.9.8/terraform_1.9.8_linux_arm64.zip"
+    sha256 "f85868798834558239f6148834884008f2722548f84034c9b0f62934b2d73ebb"
   end
 
   conflicts_with "terraform"
