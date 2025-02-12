1e6c8e44fd259c1959b5239464e61cd884968cab
yakdriverbot <f1formula.juan@gmail.com>
2025-02-12 16:12:42 +0000

    Update formulas

--- Formula/terraform_rc.rb
+++ Formula/terraform_rc.rb
@@ -6,31 +6,31 @@
 class TerraformBeta < Formula
   desc 'Terraform Latest Beta'
   homepage 'https://www.terraform.io/'
-  version '1.10.0-rc3'
+  version '1.11.0-rc1'
 
   if OS.mac? && Hardware::CPU.intel?
-    url 'https://releases.hashicorp.com/terraform/1.10.0-rc3/terraform_1.10.0-rc3_darwin_amd64.zip'
-    sha256 'f8331ee9e37a958e3922573f98d8eb04205afe64a5570b098e83f279df69eb1d'
+    url 'https://releases.hashicorp.com/terraform/1.11.0-rc1/terraform_1.11.0-rc1_darwin_amd64.zip'
+    sha256 '73085f30f6a2b36d43751de5614d5e635b0f1af6fa0341baacfac7a08bf4783b'
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url 'https://releases.hashicorp.com/terraform/1.10.0-rc3/terraform_1.10.0-rc3_darwin_arm64.zip'
-    sha256 'e03fb9184832592a773e7a352d6f326a5cd093e54212a17feed0d6eb6ad65a46'
+    url 'https://releases.hashicorp.com/terraform/1.11.0-rc1/terraform_1.11.0-rc1_darwin_arm64.zip'
+    sha256 '90612ac98a2ada9848f50506cee7148aa648c3304eaa1467af6b411fef70a721'
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url 'https://releases.hashicorp.com/terraform/1.10.0-rc3/terraform_1.10.0-rc3_linux_amd64.zip'
-    sha256 'a3abb303915638e6864793e3e717966e95f3358cdcdde7db15dae67e391c5d8e'
+    url 'https://releases.hashicorp.com/terraform/1.11.0-rc1/terraform_1.11.0-rc1_linux_amd64.zip'
+    sha256 '90a740514846c02c8ea4ceacce83d36089e86cf17b1807a29cf98bf1e89fd017'
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url 'https://releases.hashicorp.com/terraform/1.10.0-rc3/terraform_1.10.0-rc3_linux_arm.zip'
-    sha256 '334df292c6919da6d68e9c645f454ecbaf9f0df9c12a863f1934a51b2f762a61'
+    url 'https://releases.hashicorp.com/terraform/1.11.0-rc1/terraform_1.11.0-rc1_linux_arm.zip'
+    sha256 '2e8e9d20b91f5b995abc2b00762ac41e197415c598543b6d0fa18459372271ac'
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url 'https://releases.hashicorp.com/terraform/1.10.0-rc3/terraform_1.10.0-rc3_linux_arm64.zip'
-    sha256 '171eecb699aa004b138dcbf66c8d31da5c969613fb04e2ea79a25685ea01b2df'
+    url 'https://releases.hashicorp.com/terraform/1.11.0-rc1/terraform_1.11.0-rc1_linux_arm64.zip'
+    sha256 '68ba5b90aeb8ee86b00ef63ab87a550a7fab46e0f834b9803425bf3d29e00054'
   end
 
   conflicts_with 'terraform'
