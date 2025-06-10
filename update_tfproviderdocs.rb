fe22a60221640e232dffc9f965952cc3d87898c7
Dirk Avery <dirk.avery@gmail.com>
2025-06-10 11:40:20 -0400

    Update tfproviderdocs

---
+++ update_tfproviderdocs.rb
@@ -0,0 +1,73 @@
+# frozen_string_literal: true
+
+require 'net/http'
+require 'json'
+require 'digest'
+require 'uri'
+
+GITHUB_API_RELEASES = 'https://api.github.com/repos/YakDriver/tfproviderdocs/releases/latest'
+GITHUB_RELEASES_BASE = 'https://github.com/YakDriver/tfproviderdocs/releases/download'
+FORMULA_PATH = 'Formula/tfproviderdocs.rb'
+README_PATH = 'README.md'
+SUMMARY_PATH = 'update_summary.txt'
+
+# Download a file and return its SHA256 hash
+def download_and_hash(url)
+  uri = URI(url)
+  Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https') do |http|
+    resp = http.get(uri.request_uri)
+    Digest::SHA256.hexdigest(resp.body)
+  end
+end
+
+# Get the latest release info from GitHub
+resp = Net::HTTP.get(URI(GITHUB_API_RELEASES))
+release = JSON.parse(resp)
+version = release['tag_name'].gsub(/^v/, '')
+assets = release['assets']
+
+# Map platform to asset info
+platforms = {
+  'darwin_amd64' => /darwin_amd64\.tar\.gz$/,
+  'darwin_arm64' => /darwin_arm64\.tar\.gz$/,
+  'linux_amd64' => /linux_amd64\.tar\.gz$/,
+  'linux_arm64' => /linux_arm64\.tar\.gz$/
+}
+
+urls = {}
+sha256s = {}
+platforms.each do |platform, regex|
+  asset = assets.find { |a| a['name'] =~ regex }
+  next unless asset
+  url = asset['browser_download_url']
+  urls[platform] = url
+  sha256s[platform] = download_and_hash(url)
+end
+
+# Read and update the formula file
+formula = File.read(FORMULA_PATH)
+formula.gsub!(/version '\d+\.\d+\.\d+'/, "version '#{version}'")
+
+platforms.each do |platform, regex|
+  url = urls[platform]
+  sha = sha256s[platform]
+  next unless url && sha
+  url_regex = /url '.*#{platform}.*'\n\s*sha256 '[a-f0-9]{64}'/
+  formula.gsub!(url_regex, "url '#{url}'\n      sha256 '#{sha}'")
+end
+
+File.write(FORMULA_PATH, formula)
+
+# Update README.md
+if File.exist?(README_PATH)
+  readme = File.read(README_PATH)
+  readme.gsub!(/\| tfproviderdocs \| \d+\.\d+\.\d+ \|/, "| tfproviderdocs | #{version} |")
+  File.write(README_PATH, readme)
+end
+
+# Append to update_summary.txt
+File.open(SUMMARY_PATH, 'a') do |f|
+  f.puts "tfproviderdocs: #{version}"
+end
+
+puts "Updated tfproviderdocs to version #{version}"
