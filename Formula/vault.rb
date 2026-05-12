c1d4cbbc559d9564706eae31f932ffd4269a5a35
yakdriverbot <f1formula.juan@gmail.com>
2026-05-12 06:30:59 +0000

    Update formulas

--- Formula/vault.rb
+++ Formula/vault.rb
@@ -25,7 +25,7 @@ class Vault < Formula
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
     url 'https://releases.hashicorp.com/vault/2.0.0/vault_2.0.0_linux_arm.zip'
-    sha256 '6d60bd38a3a36e5f0ca31cb7d948c0388cf8a9e0a243722e10ab27d3bdb8c8f7'
+    sha256 '5bf8312cc89e80d9c8dbb5afb26c8eebeccc56854c5de6bb94a607ceda3ca65b'
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
